1ad14ca6ca68c1d9200b0053a483c87fd7ab59a1
Matthew nte <matthewnte1@gmail.com>
2020-08-09 23:45:16 +0100

    redirect affter successful blog post

--- src/app/main/blog_app/store/actions/postBlog.action.js
+++ src/app/main/blog_app/store/actions/postBlog.action.js
@@ -8,7 +8,7 @@ export const BlogPost_LOADING = 'BlogPost_LOADING';
 
 const header = fetchHeaders();
 
-export function submitBlogPost(formdata) {
+export function submitBlogPost(formdata, history) {
 	return dispatch => {
 		dispatch({
 			type: BlogPost_LOADING
@@ -24,13 +24,13 @@ export function submitBlogPost(formdata) {
 		}).then(res => res.json()).then(
 			post => {
 				if(post.message === 'Created!') {
-					console.log(post)
+					history.push(`/blog/blog_detail/${post.data.id}`)
 					Swal.fire({
 						title: 'Blog Post was sucessful',
 						text: post.message,
 						icon: 'success',
 						timer: 3000,
-					});
+          });
 					return dispatch({
 						type: BlogPost_SUCCESS
 					});
