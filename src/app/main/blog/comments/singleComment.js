aaacf07db86bba315a9c91a8199184c83af45edd
Onyetube Kosisochukwu <kosisochukwu.onyetube@fasylgh.com>
2020-08-14 02:29:09 +0000

    Completed the Blog Module.

--- src/app/main/blog/comments/singleComment.js
+++ src/app/main/blog/comments/singleComment.js
@@ -20,216 +20,224 @@ import { useAuth } from 'app/hooks/useAuth';
 const theme = createMuiTheme();
 
 theme.typography.body1 = {
-  fontSize: '16px',
+	fontSize: '16px'
 };
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginLeft: '48px',
-    marginTop: 16,
-    [theme.breakpoints.down('xs')]: {
-      marginTop: theme.spacing(1)
-    },
-  },
-  dFlex: {
-    display: 'flex',
-  },
-  spaceBtw: {
-    justifyContent: 'space-between',
-  },
-  commentBody: {
-    marginLeft: '48px',
-  },
-  content: {
-    margin: '8px 0',
-    lineHeight: 1.5,
-  },
+const useStyles = makeStyles(theme => ({
+	root: {
+		marginLeft: '48px',
+		marginTop: 16,
+		[theme.breakpoints.down('xs')]: {
+			marginTop: theme.spacing(1)
+		}
+	},
+	dFlex: {
+		display: 'flex'
+	},
+	spaceBtw: {
+		justifyContent: 'space-between'
+	},
+	commentBody: {
+		marginLeft: '48px'
+	},
+	content: {
+		margin: '8px 0',
+		lineHeight: 1.5
+	}
 }));
 
 function BlogComment(props) {
-  const classes = useStyles();
-  const dispatch = useDispatch();
+	const classes = useStyles();
+	const dispatch = useDispatch();
 
-  const [showInput, setShowInput] = useState(true);
-  const [content, setContent] = useState('');
-  const [open, setOpen] = useState(false);
-  const [isLikeComment, setIsLikeComment] = useState(props.isLike);
-  const [comment, setComment] = useState('');
-  const [input, setInput] = useState('');
-  const [likes, setLikes] = useState([]);
-  const [employeeDetails, setEmployeeDetails] = useState({});
-  const [value, setValue] = useState('');
+	const [showInput, setShowInput] = useState(true);
+	const [content, setContent] = useState('');
+	const [open, setOpen] = useState(false);
+	const [isLikeComment, setIsLikeComment] = useState(props.isLike);
+	const [comment, setComment] = useState('');
+	const [input, setInput] = useState('');
+	const [likes, setLikes] = useState([]);
+	const [employeeDetails, setEmployeeDetails] = useState({});
+	const [value, setValue] = useState('');
 
-  useEffect(() => {
-    if (props.comment) {
-      setContent(props.comment.content);
-      setComment(props.comment.content)
-      setLikes(props.comment.commentLike ? props.comment.commentLike.length : 0);
-      setEmployeeDetails(props.comment.employee);
-    }
-  }, [props.comment])
+	useEffect(() => {
+		if (props.comment) {
+			setContent(props.comment.content);
+			setComment(props.comment.content);
+			setLikes(props.comment.commentLike ? props.comment.commentLike.length : 0);
+			setEmployeeDetails(props.comment.employee);
+		}
+	}, [props.comment]);
 
-  useEffect(() => {
-    if (props.comment) {
-      setIsLikeComment(checkIfUserLikedComment(props.comment.commentLike))
-    }
-  }, [props, props.comment]);
+	useEffect(() => {
+		if (props.comment) {
+			setIsLikeComment(checkIfUserLikedComment(props.comment.commentLike));
+		}
+	}, [props, props.comment]);
 
-  const showReplyInput = (e) => {
-    e.preventDefault();
-    setShowInput(false)
-  };
+	const showReplyInput = e => {
+		e.preventDefault();
+		setShowInput(false);
+	};
 
-  const handleChange = (value) => {
-    if(value === true) {
-      setShowInput(value)
-    } else {
-      setInput(value);
-      setContent(value);
-    }
-  }
+	const handleChange = value => {
+		if (value === true) {
+			setShowInput(value);
+		} else {
+			setInput(value);
+			setContent(value);
+		}
+	};
 
-const userId = useAuth().getId;
+	const userId = useAuth().getId;
 
-function checkIfUserLikedComment(data){
-  if(userId && data) {
-    for(const i of data) {
-      if(i.employeeId === userId) {
-        return true;
-      }
-    }
-  }
-}
+	function checkIfUserLikedComment(data) {
+		if (userId && data) {
+			for (const i of data) {
+				if (i.employeeId === userId) {
+					return true;
+				}
+			}
+		}
+	}
 
-  const handleSubmitReply = () => {
-    setShowInput(true);
-    const model = {commentId: props.comment.id, content};
-    dispatch(Actions.submitBlogCommentReply(model, props.postId));
-    setContent('');
-  };
+	const handleSubmitReply = () => {
+		setShowInput(true);
+		const model = { commentId: props.comment.id, content };
+		dispatch(Actions.submitBlogCommentReply(model, props.postId));
+		setContent('');
+	};
 
-  const handleCommentEdit = () => {
-    const model = {
-      id: props.comment.id, 
-      content,
-      postId: props.postId
-    };
-    dispatch(Actions.updateAComment(model));
-    setOpen(false);
-  };
+	const handleCommentEdit = () => {
+		const model = {
+			id: props.comment.id,
+			content,
+			postId: props.postId
+		};
+		dispatch(Actions.updateAComment(model));
+		setOpen(false);
+	};
 
-  const updateCommentReply = () => {
-    console.log(props);
-    const model = {
-      id: props.postId, 
-      replyId: props.comment.id, 
-      commentId: props.commentId, 
-      content
-    };
-    dispatch(Actions.updateACommentReply(model));
-    setOpen(false);
-  };
+	const updateCommentReply = () => {
+		console.log(props);
+		const model = {
+			id: props.postId,
+			replyId: props.comment.id,
+			commentId: props.commentId,
+			content
+		};
+		dispatch(Actions.updateACommentReply(model));
+		setOpen(false);
+	};
 
-  const checkForMethodToCall = () => {
-    if (value === 'Edit comment') handleCommentEdit();
-    else updateCommentReply();
-  };
+	const checkForMethodToCall = () => {
+		if (value === 'Edit comment') handleCommentEdit();
+		else updateCommentReply();
+	};
 
-  const handleCommentDelete = () => {
-    dispatch(Actions.deleteComment(props.comment.id, props.postId));
-  };
+	const handleCommentDelete = () => {
+		dispatch(Actions.deleteComment(props.comment.id, props.postId));
+	};
 
-  const handleDeleteReply = () => {
-    dispatch(Actions.deleteCommentReply(props.comment.id, props.commentId, props.postId));
-  };
+	const handleDeleteReply = () => {
+		dispatch(Actions.deleteCommentReply(props.comment.id, props.commentId, props.postId));
+	};
 
-  const selectClickedButton = (value) => {
-    switch(value) {
-      case 'Delete comment':
-        handleCommentDelete();
-        break;
-      case 'Delete reply':
-        handleDeleteReply();
-        break;
-      default:
-        setValue(value);
-        setOpen(true);
-    }
-  };
-  
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const selectClickedButton = value => {
+		switch (value) {
+			case 'Delete comment':
+				handleCommentDelete();
+				break;
+			case 'Delete reply':
+				handleDeleteReply();
+				break;
+			default:
+				setValue(value);
+				setOpen(true);
+		}
+	};
 
-  const handleLikes = () => {
-    setIsLikeComment(!isLikeComment);
-    isLikeComment ? setLikes(prev => prev - 1) : setLikes(prev => prev + 1);
-    dispatch(Actions.likeAComment(props.comment.id, props.userId, props.comment.postId));
-  };
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  const getColor = () => !isLikeComment ? '#4d5760' : '#F44336';
+	const handleLikes = () => {
+		setIsLikeComment(!isLikeComment);
+		isLikeComment ? setLikes(prev => prev - 1) : setLikes(prev => prev + 1);
+		dispatch(Actions.likeAComment(props.comment.id, props.userId, props.comment.postId));
+	};
 
-  return (
-    <div className={`${props.commentId && classes.root}`}>
-      <SectionHeader
-        fullName={employeeDetails && `${employeeDetails.lastName} ${employeeDetails.firstName}`}
-        buttonContent={props.moreContent}
-        email={employeeDetails && employeeDetails.email}
-        blogPoster={employeeDetails && employeeDetails.id}
-        commentReplier={props.comment && props.comment.employeeId}
-        onClick={(value) => selectClickedButton(value)}
-        time={props.comment && props.comment.createdAt}
-        dp={employeeDetails && employeeDetails.profilePicture}
-      />
-      <div className={classes.commentBody}>
-        <ThemeProvider theme={theme}>
-          <Typography varaint="body1" className={classes.content}>{comment}</Typography>
-        </ThemeProvider>
-        {showInput
-          ? <div className={`${classes.dFlex} ${classes.spaceBtw}`}>
-            {props.btnContent &&<> <div>
-                <IconButton aria-label="like" onClick={handleLikes} style={{color: getColor()}} component="span">
-                  {!isLikeComment ? <FavoriteBorder /> : <Favorite />}
-                </IconButton>
-                <Typography varaint="body1" component="span" className={classes.userName}>
-                  {likes}
-                </Typography>
-              </div>
-              <Button onClick={showReplyInput}>{props.btnContent}</Button> </>}
-            </div> 
-          : <CommentInput
-              cancel="Cancel"
-              value={input}
-              onClick={() => handleSubmitReply()}
-              onChange={value => handleChange(value)}
-            />
-        }
-      </div>
-      <Dialog open={open} onClose={handleClose} fullWidth aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Update comment</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Update comment"
-            value={content}
-            type="text"
-            fullWidth
-            style={{resize: "none"}}
-            onChange={event => setContent(event.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button variant="contained" onClick={checkForMethodToCall} color="primary">
-            Update
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  )
+	const getColor = () => (!isLikeComment ? '#4d5760' : '#F44336');
+
+	return (
+		<div className={`${props.commentId && classes.root}`}>
+			<SectionHeader
+				fullName={employeeDetails && `${employeeDetails.lastName} ${employeeDetails.firstName}`}
+				buttonContent={props.moreContent}
+				email={employeeDetails && employeeDetails.email}
+				blogPoster={employeeDetails && employeeDetails.id}
+				commentReplier={props.comment && props.comment.employeeId}
+				onClick={value => selectClickedButton(value)}
+				time={props.comment && props.comment.createdAt}
+				dp={employeeDetails && employeeDetails.profilePicture}
+			/>
+			<div className={classes.commentBody}>
+				<ThemeProvider theme={theme}>
+					<Typography varaint="body1" className={classes.content}>
+						{comment}
+					</Typography>
+				</ThemeProvider>
+				{showInput ? (
+					<div className={`${classes.dFlex} ${classes.spaceBtw}`}>
+						{props.btnContent && (
+							<>
+								{' '}
+								<div>
+									<IconButton aria-label="like" onClick={handleLikes} style={{ color: getColor() }} component="span">
+										{!isLikeComment ? <FavoriteBorder /> : <Favorite />}
+									</IconButton>
+									<Typography varaint="body1" component="span" className={classes.userName}>
+										{likes}
+									</Typography>
+								</div>
+								<Button onClick={showReplyInput}>{props.btnContent}</Button>{' '}
+							</>
+						)}
+					</div>
+				) : (
+					<CommentInput
+						cancel="Cancel"
+						value={input}
+						onClick={() => handleSubmitReply()}
+						onChange={value => handleChange(value)}
+					/>
+				)}
+			</div>
+			<Dialog open={open} onClose={handleClose} fullWidth aria-labelledby="form-dialog-title">
+				<DialogTitle id="form-dialog-title">Update comment</DialogTitle>
+				<DialogContent>
+					<TextField
+						autoFocus
+						margin="dense"
+						label="Update comment"
+						value={content}
+						type="text"
+						fullWidth
+						style={{ resize: 'none' }}
+						onChange={event => setContent(event.target.value)}
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose} color="primary">
+						Cancel
+					</Button>
+					<Button variant="contained" onClick={checkForMethodToCall} color="primary">
+						Update
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
+	);
 }
 
 export default BlogComment;
