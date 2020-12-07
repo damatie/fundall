59ee79e97bb2f4955c10b89d0328eabd01ce6e64
Chinweike David <45151879+daveOnactive@users.noreply.github.com>
2020-12-07 19:48:14 +0100

    added select table row functionality to shared table component

--- src/app/main/performanceAppraisal/KPOcategoryList/KPOcategoryList.js
+++ src/app/main/performanceAppraisal/KPOcategoryList/KPOcategoryList.js
@@ -24,7 +24,7 @@ const KPOcategoryList = () => {
       content={
         <div className='p-24'>
           {
-            loading ? (
+            false ? (
               <Skeleton variant="rect" width='100%' height={400} animation="wave"/>
             ) : (
               <>
