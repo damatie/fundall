ed022be3681095a2e9c987110969631d3e6cd7a2
Chinweike David <45151879+daveOnactive@users.noreply.github.com>
2021-01-06 23:09:59 +0100

    authenticated role

--- src/app/auth/authRoles.js
+++ src/app/auth/authRoles.js
@@ -1,19 +1,14 @@
 /**
  * Authorization Roles
  */
-const authRoles = {
-	admin: ['HR', 'Hr Manager'],
-	staff: ['employee', 'Employee', 'Line Manager','HR', 'Line managers', 'Finance Manager', 'Head of department', 'Director of support service', 'HR Manager', 'Hr Manager'],
-	manager: ['HR', 'Line managers', 'Director of support service', 'HR Manager'],
-	managers: ['HR', 'Line Manager', 'Finance Manager', 'Head of department', 'Director of support service', "hr", 'HR Manager'],
-	hod: ['Head of department'],
-	user: ['admin', 'staff', 'user'],
-	employee: ["employee", "Employee"],
-	financeMnager: ["finance manager", "Finance Manager"],
-	hrManager: ["Line Manager", "Hr Manager"],
-	lineManager: ["line manager", "Line Manager"],
-	onlyGuest: [],
-	loan: ['Line managers', 'Finance Manager', 'Director of support service']
-};
+
+const roles = () => {
+	return {
+		admin: ['Hr Manager', 'Hr Admin'],
+		staff: ['Employee', 'Line Manager', 'Finance Manager', 'Hr Manager', 'Hr Admin'],
+		manager: ['Line Manager', 'Finance Manager', 'Hr Manager', 'Hr Admin'],
+	};
+}
+const authRoles = roles();
 
 export default authRoles;
