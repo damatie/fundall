ce74878f200d8ac60c7e5fd865bc3120ccdc2cb0
SirPm <odumosuoluwapm@gmail.com>
2021-08-03 16:55:10 +0100

    3 kpo screen update

---
+++ src/app/main/performanceAppraisal/KPOlist/components/BehavioralAttributeTable.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
+
+const useStyles = makeStyles(theme => ({
+	baTable: {
+		width: '90%',
+		marginBottom: '10%'
+	},
+	largeTr: {
+		width: '45%'
+	},
+	mediumTr: {
+		width: '15%'
+	},
+	normalTr: {
+		width: '12%'
+	},
+	smallTr: {
+		width: '4%'
+	},
+	th: {
+		fontSize: 13
+	},
+	td: {
+		textAlign: 'center'
+	},
+	tbodyTd: {
+		paddingTop: 3,
+		paddingBottom: 50
+	}
+}));
+
+const BehavioralAttributeTable = ({ tableData }) => {
+	const classes = useStyles();
+	const checkBoxState = {};
+
+	tableData.forEach(td => {
+		checkBoxState[td?.name + 'DevelopmentNeeded'] = false;
+		checkBoxState[td?.name + 'Satisfactory'] = false;
+		checkBoxState[td?.name + 'KeyStrength'] = false;
+		checkBoxState[td?.name + 'Leader'] = false;
+	});
+
+	// console.log(checkBoxState);
+
+	const [checkedInputBox, setCheckedInputBox] = useState(checkBoxState);
+
+	const handleChange = e => {
+		const { name, checked } = e.target;
+
+		setCheckedInputBox({
+			...checkedInputBox,
+			[name]: checked
+		});
+	};
+
+	useEffect(() => console.log(checkedInputBox, 'checked input box status'), [checkedInputBox]);
+
+	return (
+		<table className={` ${classes.baTable}`}>
+			<thead>
+				<tr>
+					<th></th>
+					<th className={` ${classes.th}`}>
+						Development Needed <br /> 1
+					</th>
+					<th className={` ${classes.th}`}>
+						Satisfactory <br /> 1.5
+					</th>
+					<th className={` ${classes.th}`}>
+						Key Strength <br /> 2
+					</th>
+					<th className={` ${classes.th}`}>
+						Leader <br /> 2.5
+					</th>
+					<th className={` ${classes.th}`}>
+						Total <br /> Score
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				{tableData.map(({ id, title, content, name }) => (
+					<tr key={id}>
+						<td className={` ${classes.largeTr} ${classes.tbodyTd}`}>
+							<strong>{title}</strong> <br />
+							{content}
+						</td>
+						<td className={` ${classes.td} ${classes.mediumTr} ${classes.tbodyTd}`}>
+							<Checkbox
+								checked={checkedInputBox.developmentNeeded}
+								onChange={handleChange}
+								inputProps={{ 'aria-label': 'primary checkbox' }}
+								name={`${name}DevelopmentNeeded`}
+							/>
+						</td>
+						<td className={` ${classes.td} ${classes.normalTr} ${classes.tbodyTd}`}>
+							<Checkbox
+								checked={checkedInputBox.satisfactory}
+								onChange={handleChange}
+								inputProps={{ 'aria-label': 'primary checkbox' }}
+								name={`${name}Satisfactory`}
+							/>
+						</td>
+						<td className={` ${classes.td} ${classes.normalTr} ${classes.tbodyTd}`}>
+							<Checkbox
+								checked={checkedInputBox.keyStrength}
+								onChange={handleChange}
+								inputProps={{ 'aria-label': 'primary checkbox' }}
+								name={`${name}KeyStrength`}
+							/>
+						</td>
+						<td className={` ${classes.td} ${classes.normalTr} ${classes.tbodyTd}`}>
+							<Checkbox
+								checked={checkedInputBox.leader}
+								onChange={handleChange}
+								inputProps={{ 'aria-label': 'primary checkbox' }}
+								name={`${name}Leader`}
+							/>
+						</td>
+						<td className={`${classes.tbodyTd}`}></td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
+};
+
+export default BehavioralAttributeTable;
--- src/app/main/performanceAppraisal/KPOlist/components/BehavioralAttributes.js
+++ src/app/main/performanceAppraisal/KPOlist/components/BehavioralAttributes.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+// import BehaviouralAttribute from './BehaviouralAttribute';
+import BehavioralAttribute from './BehavioralAttribute';
 
 const useStyles = makeStyles(theme => ({
 	behavioralAttributesDiv: {
 		display: 'flex',
 		flexDirection: 'column',
-		marginTop: '10%',
-		marginLeft: '18%'
-	},
-	behavioralAttribute: {
-		marginBottom: '6%',
-
-		'&:last-child': {
-			marginBottom: 0
-		}
-	},
-	playArrowIcon: {
-		cursor: 'pointer',
-		marginLeft: '3%'
-	},
-	baSpan: {
-		fontWeight: 700,
-		fontSize: 14
+		marginTop: '8%',
+		marginLeft: '15%'
 	}
 }));
 
@@ -32,29 +18,51 @@ const BehavioralAttributes = () => {
 	const behavioralAttributes = [
 		{
 			id: 1,
-			label: 'Commitment and Leadership'
+			label: 'Commitment and Leadership',
+			type: 'cal',
+			tableData: [
+				{
+					id: '1cal',
+					title: 'Service Focus',
+					content: 'Dedication to external and internal customer service and to exceeding customer expectations.',
+					name: 'communityAndLeadership'
+				},
+				{
+					id: '2cal',
+					title: 'Continous Improvement',
+					content: 'Utilizes every opportunity to improve performance and maximize value.',
+					name: 'continousImprovement'
+				},
+				{
+					id: '3cal',
+					title: 'Self-Development',
+					content:
+						'Motivated to acquire the knowledge and skills you require for your job and maintain your personal development.',
+					name: 'selfDevelopment'
+				}
+			]
 		},
 		{
 			id: 2,
-			label: 'Integrity'
+			label: 'Integrity',
+			type: 'itgy'
 		},
 		{
-			id: 1,
-			label: 'Teamwork'
+			id: 3,
+			label: 'Teamwork',
+			type: 'twrk'
 		},
 		{
-			id: 1,
-			label: 'Personal Drive'
+			id: 4,
+			label: 'Personal Drive',
+			type: 'pd'
 		}
 	];
 
 	return (
 		<div className={` ${classes.behavioralAttributesDiv}`}>
 			{behavioralAttributes.map(ba => (
-				<div className={` ${classes.behavioralAttribute}`} key={ba.id}>
-					<span className={` ${classes.baSpan}`}>{ba.label}</span>
-					<PlayArrowIcon className={` ${classes.playArrowIcon}`} />
-				</div>
+				<BehavioralAttribute key={ba.id} {...ba} />
 			))}
 		</div>
 	);
