9f3203965f8fa1eb547ed5775f9356cce43bd9fd
SamuelPhases <obajisamuelekene@gmail.com>
2021-11-05 16:39:08 +0100

    started the audience page and made the corrections

--- src/app/main/employee_survey/audienceGroup/deleteModal.js
+++ src/app/main/employee_survey/audienceGroup/deleteModal.js
@@ -9,7 +9,7 @@ function DeleteModal({setDeleteModal,deleteAudience,singleAudienceId,singleAudie
             <div className="pt-64 flex-col-scroll flex top-0 right-0 mx-auto w-full top-2/4 absolute">
                 <Cards className="w-1/2 mx-auto mt-96 p-32 py-56 text-center flex-col items-center">
                     <h3 className="text-xl font-semibold py-20">Are you sure you want to delete {singleAudienceItem?.title}</h3>
-                    <div className="w-10/12 mx-auto my-20 flex items-center justify-between">
+                    <div className="w-7/12 mx-auto my-20 flex items-center justify-between">
                         <Button className="px-20 py-6 text-12 font-semibold bg-red-200 cursor-pointer text-red-900" onClick={() => deleteAudience(singleAudienceId)}>Yes, I want to delete</Button>
                         <Button className="px-20 py-6 text-12 font-semibold text-white cursor-pointer bg-green-700" onClick={() => setDeleteModal(false)}>Cancel</Button>
                     </div>
