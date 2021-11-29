c87e88c58b1b3e8875fb6a1429087a3a7e98c436
edafe <edafe@cbitindustries.com>
2021-11-29 14:18:36 +0100

    edit one survey

--- src/app/main/employee_survey/component/surveyQuestion.js
+++ src/app/main/employee_survey/component/surveyQuestion.js
@@ -1,70 +1,95 @@
 
 import React from 'react'
-import { useState,useRef, useEffect } from 'react';
-import TextField from '@material-ui/core/TextField';
 import Cards from 'app/shared/cards/cards'
-import SwitchButton from '../../../shared/button/SwitchButton';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 import Radio from '@material-ui/core/Radio';
-import * as allSurveyFormActions  from '../store/actions'
 import { useDispatch, useSelector } from 'react-redux';
+import * as allSurveyFormActions  from '../store/actions'
 import reducer from '../store/reducers';
 import withReducer from 'app/store/withReducer';
+import SurveyForms from './surveyForms';
 
 
- function surveyQuestion() {
+ function surveyQuestion(props) {
 	const dispatch = useDispatch();
-	const surveyQuestions = useSelector((state => state.surveyForms.surveyFormsReducer.surveyQuestion ))
+	const stateData = useSelector((state => state.surveyForms.surveyFormsReducer ))
+
+	function handleDeleteSurveyQuestion (id) {
+		dispatch(allSurveyFormActions.deleteSurveyQuestion(id));
+	}
+
+	function handleEditOneSurveyQuestion (id, value) {
+		dispatch(allSurveyFormActions.editOneSurveyQuestion(id, value));
+	}
 
   const handleChange = (event) => {
   };
-
-	//  // Intial event
-	//  useEffect(() => {
-	//   dispatch(allSurveyFormActions.getSurveyQuestion())
-	// }, [dispatch]);
-
 	 
 	return(
 		<div className=" flex flex-col   relative ">
 
-			{ surveyQuestions.map((surveyQuestion, surveyQuestionIndex) =>
-			
-				<Cards className="w-7/12 mx-auto py-10 px-16 mb-40 rounded-20px shadow-10  break-words" key={surveyQuestionIndex}>
-					<div className="w-full" style={{fontSize:14, fontWeight:700,}}>
-					<span> {surveyQuestion.isRequired}</span>
-						{surveyQuestion.body}
-						
-					</div>
-				{	surveyQuestion.options.map((option, optionIndex) =>
-				<div className="w-full flex" style={{fontSize:14,}} key={optionIndex}> 
+			{ stateData.surveyQuestion.map((surveyQuestion, surveyQuestionIndex) =>
+			<div key={surveyQuestionIndex}>
+				{surveyQuestion.isEdit === true? 
+					<div className=" mb-32">
+						<SurveyForms surveyQuestionId={surveyQuestionIndex}/>
+					</div> : 
+					<Cards className="w-7/12 mx-auto py-10 px-16 mb-40 rounded-20px shadow-10  break-words" >
+						<div className="w-full" style={{fontSize:14, fontWeight:700,}}>
+							<span> {surveyQuestion.isRequired}</span>
+								{surveyQuestion.body}
+								{ surveyQuestion.isRequired === true? 
+								<span style={{color:'#FF3030'}}> * </span>
+								:
+								""	
+								}
+								
+						</div>
+						{	surveyQuestion.options.map((option, optionIndex) =>
+							<div className="w-full flex" style={{fontSize:14,}} key={optionIndex}> 
 
-					{surveyQuestion.selected  ==='multiChoice'? 
-					<span>
-					<Radio
-						name="radio-button"
-						inputProps={{ 'aria-label': 'A' }}
-      />
-			</span>
-					: surveyQuestion.selected ==='checkBox'? 	
-					<span>
-							<Checkbox
-								inputProps={{ 'aria-label': 'primary checkbox' }}
-							/>
+							{surveyQuestion.selected  ==='multiChoice'? 
+							<span>
+							<Radio
+								name="radio-button"
+								inputProps={{ 'aria-label': 'A' }}
+					/>
 					</span>
-					 : '' }
-				
-						<span className="  leading-10 pt-2">
-							{option.name}
-						</span>
-					</div>
-						)
-					}
-					<span className=" float-right  cursor-pointer font-800 "> Delete</span>
-					<span className=" float-right  cursor-pointer font-800 pr-16"> Edit</span>
-					
-			</Cards>
+							: surveyQuestion.selected ==='checkBox'? 	
+							<span>
+									<Checkbox
+										inputProps={{ 'aria-label': 'primary checkbox' }}
+									/>
+							</span>
+							: '' }
+						
+								<span className="  leading-10 pt-2">
+									{option.name}
+								</span>
+							</div>
+							)
+						}
+						{ stateData.isEdit === false?
+							<div className=" w-full"> 
+							<span className=" float-right bg-red-100 px-6 py-2 rounded-lg cursor-pointer" onClick={()=>handleDeleteSurveyQuestion(surveyQuestionIndex)}>
+								<DeleteOutlineIcon style={{ color:'#FF3030',fontSize: 16}}/>
+							</span>
+							<span
+							className=" float-right bg-light-blue-50 rounded-lg px-6 py-2  cursor-pointer font-800 mr-16" 
+							onClick={()=> handleEditOneSurveyQuestion(surveyQuestionIndex,!surveyQuestion.isEdit)}
+							>
+								<EditIcon  style={{ color:'#62DAFC',fontSize: 16}} /> 
+							</span>
+						</div>
+						:
+						""
+						}
+					</Cards>
+ 				}
+			</div>
+			
 			)
 			}
 		</div>
