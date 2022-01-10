6b1d8092bea31a9b485113f7ec4cf6aa742b1ba5
SamuelPhases <52789789+SamuelPhases@users.noreply.github.com>
2022-01-10 10:26:11 +0100

    fix icons and bg

--- src/app/main/employee_survey/shared/surveyCard.js
+++ src/app/main/employee_survey/shared/surveyCard.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 import EditIcon from '@material-ui/icons/Edit';
 
 
@@ -23,19 +24,19 @@ function SurveyCard({surveyCardItem,deleteSurvey,index,setOpenSurvey,populateSur
             <div className="md:w-1/3 w-full space-y-4 flex flex-col justify-between">
                 <p className="text-blue-400 text-14 pt-8 text-right">Survey sent to {surveyCardItem?.noOfRecipients ? surveyCardItem?.noOfRecipients : 0}</p>
                 {surveyCardItem?.editable && (
-                    <div className='flex justify-end space-x-8'>
-                        <button
-                            className="bg-blue-200 text-blue-700 cursor-pointer rounded-sm py-4 px-6"
+                    <div className='flex justify-end'>
+                        <span
+                            className=" float-right bg-light-blue-50 rounded-lg px-6 py-2  cursor-pointer font-800 mr-16" 
                             onClick={() => populateSurvey(surveyCardItem,surveyCardItem?.id)}
                         >
-                            <EditIcon  className="text-18" />
-                        </button>
-                        <button
-                            className="bg-red-200 cursor-pointer rounded-sm py-4 px-6 text-red-700"
+                            <EditIcon  style={{ color:'#62DAFC',fontSize: 16}} /> 
+                        </span>
+                        <span 
+                            className=" float-right bg-red-100 px-6 py-2 rounded-lg cursor-pointer"
                             onClick={(id) => deleteSurvey(surveyCardItem,surveyCardItem?.id,index)}
                         >
-                            <DeleteIcon className='text-18' />
-                        </button>
+                            <DeleteOutlineIcon style={{ color:'#FF3030',fontSize: 16}}/>
+                        </span>
                     </div>
                 )}
                 {/*  */}
